7e018da6e14ff8b6bce0626238bef13f2817cd2a
Abhi Khokhar <abhikhokhar2004@gmail.com>
2026-01-24 22:35:05 +0530

    Solve array problem

--- array/level1.cpp
+++ array/level1.cpp
@@ -43,17 +43,29 @@ int main(){
 
     //Print second largest and second smallest element of an array
     //tc= O(n)
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i=0; i<n; i++){
-        cin >> arr[i];
-    }
-    int sLargest = secondLargest(arr, n);
-    cout << "second largst element is : " << sLargest << endl;
-    int sSmallest = secondSmallest(arr,n);
-    cout << "second smallest element is : " << sSmallest;
+    // int n;
+    // cin >> n;
+    // int arr[n];
+    // for (int i=0; i<n; i++){
+    //     cin >> arr[i];
+    // }
+    // int sLargest = secondLargest(arr, n);
+    // cout << "second largst element is : " << sLargest << endl;
+    // int sSmallest = secondSmallest(arr,n);
+    // cout << "second smallest element is : " << sSmallest;
 
-    
+    //left rotate an array by one position
+    //tc O(n)
+    //sc O(n)-  space we are using in the algorithm
+    //sc O(1)- extra space we are using
+    int arr[] = {3,5,7,2,8};
+    int temp = arr[0];
+    for (int i=1; i<5;i++){
+        arr[i-1] = arr[i];
+    }
+    arr[4] = temp;
+    for(int i=0; i<5;i++){
+        cout << arr[i] << " ";
+    }
 }
  
