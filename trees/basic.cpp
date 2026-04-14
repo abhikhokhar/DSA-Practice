c7e7c32cf82e00c27bece226abeeeeffb9623aa7
Abhi Khokhar <abhikhokhar2004@gmail.com>
2026-04-14 12:14:06 +0530

    Started with Trees

---
+++ trees/basic.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    public:
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+int countNodes(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int leftNodes = countNodes(root->left);
+    int rightNodes = countNodes(root->right);
+    return leftNodes + rightNodes + 1;
+}
+
+int main(){
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    int nodes = countNodes(root);
+    cout << nodes << endl;
+}
